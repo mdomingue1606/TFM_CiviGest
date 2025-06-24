afc9e762cba55b69bc806c103e23e4e8eafd4c79
mdomingue1606 <mdomingue1606@alumno.uned.es>
2025-06-24 13:15:26 +0200

    Cambio direcciones archivo powershell

--- Despliegue CiviGest.ps1
+++ Despliegue CiviGest.ps1
@@ -1,13 +1,13 @@
 ﻿& '.\parar MySQL.ps1'
 
 # Backend
-docker build -t civigest-basic:lastest C:\Users\carme\source\repos\BasicGest
-docker build -t civigest-library:lastest C:\Users\carme\source\repos\LibraryGest
-docker build -t civigest-member:lastest C:\Users\carme\source\repos\MemberGest
-docker build -t civigest-auth:lastest C:\Users\carme\source\repos\AuthUtility
-docker build -t civigest-notification:lastest C:\Users\carme\source\repos\NotificationUtility
+docker build -t civigest-basic:lastest .\BasicGest
+docker build -t civigest-library:lastest .\LibraryGest
+docker build -t civigest-member:lastest .\MemberGest
+docker build -t civigest-auth:lastest .\AuthUtility
+docker build -t civigest-notification:lastest .\NotificationUtility
 
 # Frontend
-docker build -t civigest-frontend:lastest C:\Users\carme\OneDrive\Documentos\CiviGest\civigest
+docker build -t civigest-frontend:lastest .\frontend-civigest
 
 docker-compose up -d
